014e3fa4e48aa047f856259885d2a9c7bbe0701b
Jón Levy <nonni@nonni.cc>
2016-02-29 22:38:40 +0000

    clear world and survival demo

---
+++ demos/survival.py
@@ -0,0 +1,33 @@
+from mcpi.minecraft import Minecraft, Vec3
+from mcpi import block
+
+import time
+import random
+
+mc = Minecraft.create()
+
+def render_tiles(pos_start, pos_end, tiles):
+    
+    x_tiles = list(range(pos_start.x, pos_end.x))
+    z_tiles = list(range(pos_start.z, pos_end.z))
+    y_tiles = list(range(pos_start.y, pos_end.y))
+    #y = pos.y - 1
+    
+    for z in z_tiles:
+        for x in x_tiles:
+            for y in y_tiles:
+                random_block = random.choice(tiles)
+                mc.setBlock(x, y, z, random_block)
+                
+
+        
+    
+start = -128
+end = 128
+
+# Vec3() býr hlut með hnitum
+world_start = Vec3(start, 0, start)
+world_end = Vec3(end, 11, end)
+
+render_tiles(world_start, world_end, [0, 0, 0, 4, 50])
+    
