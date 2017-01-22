914023a8580cc21d668c493e76e67fcbc6d01277
Jón Levy <nonni@nonni.cc>
2017-01-22 08:23:07 +0000

    breytti conf stillingum

--- source/conf.py
+++ source/conf.py
@@ -57,7 +57,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'Python seminar for Minecraft'
+project = 'Koder seminars'
 #copyright = '2016, Jón Levy'
 author = 'Jon Levy'
 
